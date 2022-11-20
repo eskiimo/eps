07dc119b9ec3508b2d71a2e9f6a8ce09f40dd749
eskiimo <kareeemgm@gmail.com>
2022-11-20 23:43:04 +0200

    commit for the day sorry

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import ResponsiveAppBar from './components/nav/NavBar';
-import HomePage from './pages/homepage';
-import ContactPage from './pages/contactpage';
-import ServicePage from './pages/ServicePage';
+import ResponsiveAppBar from "./components/nav/NavBar";
+import HomePage from "./pages/homepage";
+import ContactPage from "./pages/contactpage";
+import ServicePage from "./pages/ServicePage";
 
 const App = () => {
-   return (
-      <div className='app'>
-         <Router>
-            <ResponsiveAppBar />
-            <div className="content">
-               <Routes>
-                  <Route path="/" element={<HomePage />}></Route>
-                  <Route path="/contact" element={<ContactPage />}></Route>
-                  <Route path="/services/:serviceId" element={<ServicePage />}></Route>
-               </Routes>
-            </div>
-         </Router>
-      </div>
-   );
+  return (
+    <div className="app">
+      <Router>
+        <ResponsiveAppBar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/contact" element={<ContactPage />}></Route>
+            <Route
+              path="/services/:serviceId"
+              element={<ServicePage />}
+            ></Route>
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
--- src/components/jobcard/jobcard.js
+++ src/components/jobcard/jobcard.js
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Card, CardBody, CardImg, CardTitle, CardText } from 'reactstrap';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Card, CardBody, CardImg, CardTitle, CardText } from "reactstrap";
 
-import './jobcard.css';
+import "./jobcard.css";
 
 const JobCard = (props) => {
-   const nav = useNavigate()
-   var cropped = props.jobDesc.substring(0, 50) + '...';
-   return (
-      <div className="col-sm-10 col-md-3 whole-card">
-         <Card onClick={()=>{nav(`/services/${props.jobTitle}`,{state:{props}})}}>
-            <CardImg
-               alt="Card image cap"
-               src={props.jobImage}
-               top
-               width="100%"
-            />
-            <CardBody>
-               <CardTitle className="txt-secondary" tag="h5">
-                  {props.jobTitle}
-               </CardTitle>
+  const nav = useNavigate();
+  var cropped = props.jobDesc.substring(0, 50) + "...";
+  return (
+    <div className="col-sm-10 col-md-5 whole-card">
+      <Card
+        onClick={() => {
+          nav(`/services/${props.jobTitle}`, { state: { props } });
+        }}
+      >
+        <CardImg alt="Card image cap" src={props.jobImage} top width="100%" />
+        <CardBody>
+          <CardTitle className="txt-secondary" tag="h5">
+            {props.jobTitle}
+          </CardTitle>
 
-               <CardText className='card-text'>{cropped}</CardText>
-            </CardBody>
-         </Card>
-      </div>
-   );
+          <CardText className="card-text">{cropped}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
 };
 
 export default JobCard;
