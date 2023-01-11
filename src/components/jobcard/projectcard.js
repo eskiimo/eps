5941db4c3f18eefef71167679635fa953d5c3331
Kareem Kamal <34511976+eskiimo@users.noreply.github.com>
2023-01-11 22:58:29 +0200

    H

--- src/components/jobcard/projectcard.js
+++ src/components/jobcard/projectcard.js
@@ -4,10 +4,10 @@ const ProjectCards = (props) => {
   return props.list.map((item) => (
     <div
       key={item.title}
-      className=" max-w-xs xs:w-1/6 md:w-1/4 flex flex row justify-center p-0 m-0"
+      className=" max-w-xs w-1/6 md:w-1/4 flex flex row justify-center p-0 m-0"
     >
       <img
-        className="xs:w-2/6 object-scale-down"
+        className="w-2/6 object-scale-down"
         alt="Card image "
         src={item.image}
       />
